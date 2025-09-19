7457a7f9775c25f20ab682631ead993d8126b2d0
Tomas Igarza <tomasigarza99@gmail.com>
2025-09-18 23:07:29 -0300

    Correcciones para estilos gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  // These settings are crucial for GitHub Pages:
-  basePath: process.env.NODE_ENV === "production" ? "/liga-ttnb.github.io" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/liga-ttnb.github.io/" : "",
-  // This ensures links work when users navigate directly to inner pages
+  images: {
+    unoptimized: true,
+  },
   trailingSlash: true,
 };
 
